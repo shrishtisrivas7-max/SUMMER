3bd6dba4515eff1b0c7b6f9d62732813ef7b6ef6
shrishti <shrishtisrivas7@gmail.com>
2026-05-29 22:24:59 +0530

    Update day1.c

--- day1.c
+++ day1.c
@@ -1,1 +1,71 @@
+//1
+#include <stdio.h>
+ main() {
+   int n, sum = 0;
 
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
+
+//2
+
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+
+    return 0;
+}
+
+//3
+#include <stdio.h>
+
+int main() {
+    int n;
+    long long fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    printf("Factorial = %lld", fact);
+
+    return 0;
+}
+
+//4
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(n != 0) {
+        n = n / 10;
+        count++;
+    }
+
+    printf("Number of digits = %d", count);
+
+    return 0;
+}
